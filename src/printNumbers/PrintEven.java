5ac922c6110ffc7061259ec06019e4f8d86ce7b9
Thejaswini Vadlakonda <vkd.teja@gmail.com>
2022-05-10 15:19:34 -0400

    First commit

---
+++ src/printNumbers/PrintEven.java
@@ -0,0 +1,14 @@
+package printNumbers;
+
+public class PrintEven {
+
+	public static void main(String[] args) {
+		int n = 200;
+		for (int i = 0; i <= n; i++) {
+			if (i % 2 == 0)
+				System.out.println(i);
+
+		}
+
+	}
+}
---
+++ src/printNumbers/PrintOdd.java
@@ -0,0 +1,14 @@
+package printNumbers;
+
+public class PrintOdd {
+
+	public static void main(String[] args) {
+		int n = 200;
+		for (int i = 0; i <= n; i++) {
+			if (i % 2 != 0)
+				System.out.println(i);
+		}
+
+	}
+
+}
